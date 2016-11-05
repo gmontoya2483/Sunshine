47a53b146e65915409589de9dcb0425bdc0e3d91
gmontoya2483 <gabi.montoya@gmail.com>
2016-11-05 21:48:29 +0000

    Project 3 - Are we Offline

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -32,6 +32,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.android.sunshine.app.data.WeatherContract;
 import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
@@ -255,6 +256,7 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
             // to, do so now.
             mListView.smoothScrollToPosition(mPosition);
         }
+        updateEmptyView();
 
 
     }
@@ -297,6 +299,24 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
                 }
             }
 
+        }
+    }
+
+
+    /*
+        Updates the empty List view with contextually relevant information that the user can use to determine why they aren´t seeing weather.
+     */
+    private void updateEmptyView(){
+        if (mForecastAdapter.getCount()==0){
+            TextView tv=(TextView) getView().findViewById(R.id.listview_forecast_empty);
+            if(tv!=null){
+                // if cursor is empty, why? do we have an invalid location
+                int message=R.string.empty_forecast_list;
+                if(!Utility.isNetworkAvailable(getActivity())){
+                    message=R.string.empty_forecast_list_no_network;
+                }
+                tv.setText(message);
+            }
         }
     }
 
